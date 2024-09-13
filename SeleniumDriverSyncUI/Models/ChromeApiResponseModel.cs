effd5b3e6496e30efac5c893720713c891f1f6b8
Özgür Ertürk <ozgur.erturk@genrpa.com>
2024-09-13 20:32:58 +0300

    Add project files.

---
+++ SeleniumDriverSyncUI/Models/ChromeApiResponseModel.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumDriverSync.Models
+{
+    public class ChromeApiResponseModel
+    {
+        public DateTime Timestamp { get; set; }
+        public List<Versions>? Versions { get; set; }
+    }
+    public class Versions
+    {
+        public string? Version { get; set; }
+        public string? Revision { get; set; }
+        public Downloads? Downloads { get; set; }
+    }
+
+    public class Downloads
+    {
+        public List<Chrome>? Chrome { get; set; }
+        public List<ChromeDriver>? Chromedriver { get; set; }
+
+        [JsonProperty("chrome-headless-shell")]
+        public List<ChromeHeadlessShell> Chromeheadlessshell { get; set; }
+    }
+    public class Chrome
+    {
+        public string? Platform { get; set; }
+        public string? Url { get; set; }
+    }
+
+    public class ChromeDriver
+    {
+        public string? Platform { get; set; }
+        public string? Url { get; set; }
+    }
+
+    public class ChromeHeadlessShell
+    {
+        public string? Platform { get; set; }
+        public string? Url { get; set; }
+    }
+}
---
+++ SeleniumDriverSyncUI/Views/SyncingMainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeleniumDriverSyncUI.Views
+{
+    /// <summary>
+    /// Interaction logic for SyncingMainPage.xaml
+    /// </summary>
+    public partial class SyncingMainPage : Window
+    {
+        public SyncingMainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
